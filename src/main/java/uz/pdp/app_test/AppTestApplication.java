35dd51ca5656eed13e7c1b5b8d2d25c5f15cdfa3
Azizbek Komilov <kazizbek877@gmail.com>
2022-03-02 12:22:10 +0500

    start

---
+++ src/main/java/uz/pdp/app_test/AppTestApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.app_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTestApplication.class, args);
+    }
+
+}
